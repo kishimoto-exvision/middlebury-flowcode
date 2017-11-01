d9f6439a962a38f935321069775ab0b3b64180ab
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-11-01 16:02:46 +0900

    Replace imageLib with OpenCV

---
+++ colorflow/FlowImage.cpp
@@ -0,0 +1,286 @@
+// flow_io.cpp
+//
+// read and write our simple .flo flow file format
+
+// ".flo" file format used for optical flow evaluation
+//
+// Stores 2-band float image for horizontal (u) and vertical (v) flow components.
+// Floats are stored in little-endian order.
+// A flow value is considered "unknown" if either |u| or |v| is greater than 1e9.
+//
+//  bytes  contents
+//
+//  0-3     tag: "PIEH" in ASCII, which in little endian happens to be the float 202021.25
+//          (just a sanity check that floats are represented correctly)
+//  4-7     width as an integer
+//  8-11    height as an integer
+//  12-end  data (width*height*2*4 bytes total)
+//          the float values for u and v, interleaved, in row order, i.e.,
+//          u[row0,col0], v[row0,col0], u[row0,col1], v[row0,col1], ...
+//
+
+
+// first four bytes, should be the same in little endian
+#define TAG_FLOAT 202021.25  // check for this when READING the file
+#define TAG_STRING "PIEH"    // use this when WRITING the file
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <exception>
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include "FlowImage.h"
+
+using namespace std;
+
+void FlowImage::ReadFromFloFile(const char* file_path)
+{
+    if (file_path == "")
+        throw CError("ReadFlowFile: empty file_path");
+
+    const char *dot = strrchr(file_path, '.');
+    if (strcmp(dot, ".flo") != 0)
+        throw CError("ReadFlowFile (%s): extension .flo expected", file_path);
+
+    FILE* stream = fopen(file_path, "rb");
+    if (stream == 0)
+        throw CError("ReadFlowFile: could not open %s", file_path);
+
+    int width, height;
+    float tag;
+
+    if ((int)fread(&tag, sizeof(float), 1, stream) != 1 ||
+        (int)fread(&width, sizeof(int), 1, stream) != 1 ||
+        (int)fread(&height, sizeof(int), 1, stream) != 1)
+        throw CError("ReadFlowFile: problem reading file %s", file_path);
+
+    if (tag != TAG_FLOAT) // simple test for correct endian-ness
+        throw CError("ReadFlowFile(%s): wrong tag (possibly due to big-endian machine?)", file_path);
+
+    // another sanity check to see that integers were read correctly (99999 should do the trick...)
+    if (width < 1 || width > 99999)
+        throw CError("ReadFlowFile(%s): illegal width %d", file_path, width);
+
+    if (height < 1 || height > 99999)
+        throw CError("ReadFlowFile(%s): illegal height %d", file_path, height);
+
+    int nBands = 2;
+    FlowMatF32C2 = cv::Mat(height, width, CV_32FC2);
+
+    //printf("reading %d x %d x 2 = %d floats\n", width, height, width*height*2);
+    int n = nBands * width * height;
+    if ((int)fread(FlowMatF32C2.data, sizeof(float), n, stream) != n)
+        throw CError("ReadFlowFile(%s): file is too short", file_path);
+
+    if (fgetc(stream) != EOF)
+        throw CError("ReadFlowFile(%s): file is too long", file_path);
+
+    fclose(stream);
+}
+
+// write a 2-band image into flow file 
+void FlowImage::WriteToFloFile(const char* file_path)
+{
+    if (file_path == "")
+        throw CError("WriteFlowFile: empty file_path");
+
+    const char *dot = strrchr(file_path, '.');
+    if (dot == NULL)
+        throw CError("WriteFlowFile: extension required in file_path '%s'", file_path);
+
+    if (strcmp(dot, ".flo") != 0)
+        throw CError("WriteFlowFile: file_path '%s' should have extension '.flo'", file_path);
+
+    int width = FlowMatF32C2.rows;
+    int height = FlowMatF32C2.cols;
+    int nBands = FlowMatF32C2.channels();
+
+    if (nBands != 2)
+        throw CError("WriteFlowFile(%s): image must have 2 bands", file_path);
+
+    FILE *stream = fopen(file_path, "wb");
+    if (stream == 0)
+        throw CError("WriteFlowFile: could not open %s", file_path);
+
+    // write the header
+    fprintf(stream, TAG_STRING);
+    if ((int)fwrite(&width, sizeof(int), 1, stream) != 1 ||
+        (int)fwrite(&height, sizeof(int), 1, stream) != 1)
+        throw CError("WriteFlowFile(%s): problem writing header", file_path);
+
+    // write the rows
+    int n = nBands * width * height;
+    if ((int)fwrite(FlowMatF32C2.data, sizeof(float), n, stream) != n)
+        throw CError("WriteFlowFile(%s): problem writing data", file_path);
+
+    fclose(stream);
+}
+
+inline void FlowImage::SetColorWheelColor(int r, int g, int b, int k)
+{
+    ColorWheel[k][0] = r;
+    ColorWheel[k][1] = g;
+    ColorWheel[k][2] = b;
+}
+
+void FlowImage::MakeColorWheel()
+{
+    // relative lengths of color transitions:
+    // these are chosen based on perceptual similarity
+    // (e.g. one can distinguish more shades between red and yellow 
+    //  than between yellow and green)
+    int RY = 15;
+    int YG = 6;
+    int GC = 4;
+    int CB = 11;
+    int BM = 13;
+    int MR = 6;
+    ColorsCount = RY + YG + GC + CB + BM + MR;
+    //printf("ColorsCount = %d\n", ColorsCount);
+    if (ColorsCount > ColorsCountMax)
+        exit(1);
+    int i;
+    int k = 0;
+    for (i = 0; i < RY; i++) SetColorWheelColor(255, 255 * i / RY, 0, k++);
+    for (i = 0; i < YG; i++) SetColorWheelColor(255 - 255 * i / YG, 255, 0, k++);
+    for (i = 0; i < GC; i++) SetColorWheelColor(0, 255, 255 * i / GC, k++);
+    for (i = 0; i < CB; i++) SetColorWheelColor(0, 255 - 255 * i / CB, 255, k++);
+    for (i = 0; i < BM; i++) SetColorWheelColor(255 * i / BM, 0, 255, k++);
+    for (i = 0; i < MR; i++) SetColorWheelColor(255, 0, 255 - 255 * i / MR, k++);
+}
+
+inline void FlowImage::GetColor(float fx, float fy, unsigned char *pix)
+{
+    if (ColorsCount == 0) { MakeColorWheel(); }
+
+    float rad = sqrt(fx * fx + fy * fy);
+    float a = atan2(-fy, -fx) / 3.14159265358979323846;
+    float fk = (a + 1.0) / 2.0 * (ColorsCount - 1);
+    int k0 = (int)fk;
+    int k1 = (k0 + 1) % ColorsCount;
+    float f = fk - k0;
+    //f = 0; // uncomment to see original color wheel
+    for (int b = 0; b < 3; b++)
+    {
+        float col0 = ColorWheel[k0][b] / 255.0;
+        float col1 = ColorWheel[k1][b] / 255.0;
+        float col = (1 - f) * col0 + f * col1;
+        if (rad <= 1)
+            col = 1 - rad * (1 - col); // increase saturation with radius
+        else
+            col *= .75; // out of range
+        pix[2 - b] = (int)(255.0 * col);
+    }
+}
+
+void FlowImage::GetReferenceImage(cv::Mat& reference_image, float flow_l2_distance_max, int image_one_side_length)
+{
+    // make picture a bit bigger to show out-of-range coding
+    float range = 1.04 * flow_l2_distance_max;
+    try
+    {
+        reference_image.zeros(image_one_side_length, image_one_side_length, CV_8UC3);
+        FlowImage util;
+
+        int s2 = image_one_side_length / 2;
+        for (int y = 0; y < image_one_side_length; y++) {
+            for (int x = 0; x < image_one_side_length; x++) {
+                float fx = (float)x / (float)s2 * range - range;
+                float fy = (float)y / (float)s2 * range - range;
+                // make black coordinate axes
+                if (x == s2 || y == s2)
+                    continue;
+                util.GetColor(fx / flow_l2_distance_max, fy / flow_l2_distance_max, reference_image.at<uchar*>(y, x));
+            }
+        }
+        int ir = (int)flow_l2_distance_max;
+        int ticksize = image_one_side_length < 120 ? 1 : 2;
+        for (int k = -ir; k <= ir; k++) {
+            int ik = (int)(k / range * s2) + s2;
+            for (int t = -ticksize; t <= ticksize; t++) {
+                uchar *pix;
+                pix = reference_image.at<uchar*>(s2 + t, ik, 0);
+                pix[0] = pix[1] = pix[2] = 0;
+                pix = reference_image.at<uchar*>(ik, s2 + t, 0);
+                pix[0] = pix[1] = pix[2] = 0;
+            }
+        }
+    }
+    catch (exception &err)
+    {
+        fprintf(stderr, err.what());
+        fprintf(stderr, "\n");
+    }
+}
+
+float FlowImage::GetFlowL2DistanceMaximum()
+{
+    int width = FlowMatF32C2.cols;
+    int height = FlowMatF32C2.rows;
+
+    float maxx = -1e10, maxy = -1e10;
+    float minx = 1e10, miny = 1e10;
+    float ret = -1;
+    for (int y = 0; y < height; y++)
+    {
+        int row_offset = y * width;
+        float* flow_row_ptr = ((float*)FlowMatF32C2.data) + 2 * row_offset;
+        for (int x = 0; x < width; x++)
+        {
+            float fx = -flow_row_ptr[2 * x + 0];
+            float fy = -flow_row_ptr[2 * x + 1];
+            if (IsUnknownFlow(fx, fy)) { continue; }
+            maxx = __max(maxx, fx);
+            maxy = __max(maxy, fy);
+            minx = __min(minx, fx);
+            miny = __min(miny, fy);
+            float rad = sqrt(fx * fx + fy * fy);
+            ret = __max(ret, rad);
+        }
+    }
+
+    if (IsVerbose)
+    {
+        printf("max motion: %.4f  motion range: u = %.3f .. %.3f;  v = %.3f .. %.3f\n", ret, minx, maxx, miny, maxy);
+    }
+
+    // if flow == 0 everywhere
+    if (ret == 0)
+    {
+        printf("ret == 0.  set ret to 1.\n");
+        ret = 1;
+    }
+    return ret;
+}
+
+void FlowImage::GetColorFlowImage(cv::Mat& color_flow_image, float flow_l2_distance_max)
+{
+    if (flow_l2_distance_max <= 0) { throw CError("flow_l2_distance_max must be larger than 0"); }
+    int width = FlowMatF32C2.cols;
+    int height = FlowMatF32C2.rows;
+    if (width <= 0 || height <= 0) { throw CError("FlowMatF32C2 is invalid"); }
+    if (color_flow_image.rows != FlowMatF32C2.rows || color_flow_image.cols != FlowMatF32C2.cols)
+    {
+        color_flow_image.zeros(height, width, CV_8UC3);
+    }
+
+    for (int y = 0; y < height; y++)
+    {
+        int row_offset = y * width;
+        float* flow_row_ptr = ((float*)FlowMatF32C2.data) + 2 * row_offset;
+        uchar* rgb_row_ptr = color_flow_image.data + 3 * row_offset;
+        for (int x = 0; x < width; x++)
+        {
+            float fx = -flow_row_ptr[2 * x + 0];
+            float fy = -flow_row_ptr[2 * x + 1];
+            uchar* pix = rgb_row_ptr + 3 * x;
+            if (IsUnknownFlow(fx, fy)) { pix[0] = pix[1] = pix[2] = 0; }
+            else
+            {
+                GetColor(fx / flow_l2_distance_max, fy / flow_l2_distance_max, pix);
+            }
+        }
+    }
+}
--- colorflow/colorflow.cpp
+++ colorflow/colorflow.cpp
@@ -1,115 +1,37 @@
 #include <stdio.h>
 #include <math.h>
-#include "imageLib.h"
-#include "flowIO.h"
-#include "colorcode.h"
-
 #include <string>
+#include "FlowImage.h"
 
-int verbose = 1;
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
-void ExportColorTestImage(float truerange, char* outname, int size)
-{
-    float range = 1.04 * truerange; // make picture a bit bigger to show out-of-range coding
-    CShape sh(size, size, 3);
-    CByteImage out(sh);
-
-    int s2 = size / 2;
-    for (int y = 0; y < size; y++) {
-        for (int x = 0; x < size; x++) {
-            float fx = (float)x / (float)s2 * range - range;
-            float fy = (float)y / (float)s2 * range - range;
-            if (x == s2 || y == s2) // make black coordinate axes
-                continue;
-            uchar *pix = &out.Pixel(x, y, 0);
-            //fx = rintf(fx);
-            //fy = rintf(fy);
-            computeColor(fx / truerange, fy / truerange, pix);
-        }
-    }
-    int ir = (int)truerange;
-    int ticksize = size < 120 ? 1 : 2;
-    for (int k = -ir; k <= ir; k++) {
-        int ik = (int)(k / range * s2) + s2;
-        for (int t = -ticksize; t <= ticksize; t++) {
-            uchar *pix;
-            pix = &out.Pixel(ik, s2 + t, 0); pix[0] = pix[1] = pix[2] = 0;
-            pix = &out.Pixel(s2 + t, ik, 0); pix[0] = pix[1] = pix[2] = 0;
-        }
-    }
-
-    WriteImageVerb(out, outname, verbose);
-}
-
-void MotionToColor(CFloatImage motim, CByteImage &colim, float maxmotion)
-{
-    CShape sh = motim.Shape();
-    int width = sh.width, height = sh.height;
-    colim.ReAllocate(CShape(width, height, 3));
-    int x, y;
-    // determine motion range:
-    float maxx = -999, maxy = -999;
-    float minx = 999, miny = 999;
-    float maxrad = -1;
-    for (y = 0; y < height; y++) {
-        for (x = 0; x < width; x++) {
-            float fx = motim.Pixel(x, y, 0);
-            float fy = motim.Pixel(x, y, 1);
-            if (unknown_flow(fx, fy))
-                continue;
-            maxx = __max(maxx, fx);
-            maxy = __max(maxy, fy);
-            minx = __min(minx, fx);
-            miny = __min(miny, fy);
-            float rad = sqrt(fx * fx + fy * fy);
-            maxrad = __max(maxrad, rad);
-        }
-    }
-    printf("max motion: %.4f  motion range: u = %.3f .. %.3f;  v = %.3f .. %.3f\n",
-        maxrad, minx, maxx, miny, maxy);
-
-
-    if (maxmotion > 0) // i.e., specified on commandline
-        maxrad = maxmotion;
-
-    if (maxrad == 0) // if flow == 0 everywhere
-        maxrad = 1;
-
-    if (verbose)
-        fprintf(stderr, "normalizing by %g\n", maxrad);
-
-    for (y = 0; y < height; y++) {
-        for (x = 0; x < width; x++) {
-            float fx = motim.Pixel(x, y, 0);
-            float fy = motim.Pixel(x, y, 1);
-            uchar *pix = &colim.Pixel(x, y, 0);
-            if (unknown_flow(fx, fy)) {
-                pix[0] = pix[1] = pix[2] = 0;
-            }
-            else {
-                computeColor(fx / maxrad, fy / maxrad, pix);
-            }
-        }
-    }
-}
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     try
     {
+        FlowImage flow_image;
+        flow_image.IsVerbose = true;
         int argn = 1;
         if (argc > 1 && argv[1][0] == '-' && argv[1][1] == 'c') {
             argn++;
             float trueRange = (argn < argc) ? atof(argv[argn++]) : 10;
             int imageSize = (argn < argc) ? atoi(argv[argn++]) : 151;
             char outputPngImageFilename[256] = { 0 };
-            sprintf(outputPngImageFilename, "colortest_truerange=%f_size=%d.png", trueRange, imageSize);
-            ExportColorTestImage(trueRange, outputPngImageFilename, imageSize);
+
+            {
+                cv::Mat test_image;
+                sprintf(outputPngImageFilename, "colortest_truerange=%f_size=%d.png", trueRange, imageSize);
+                flow_image.GetReferenceImage(test_image, trueRange, imageSize);
+                cv::imwrite(outputPngImageFilename, test_image);
+            }
         }
         else
         {
             if (argc > 1 && argv[1][0] == '-' && argv[1][1] == 'q') {
-                verbose = 0;
                 argn++;
             }
             if (argn + 1 <= argc && argc <= argn + 3) {
@@ -127,19 +49,10 @@ int main(int argc, char *argv[])
                 argn++;
 
                 {
-                    CFloatImage im, fband;
-                    ReadFlowFile(im, flowname);
-                    CByteImage band, outim;
-                    CShape sh = im.Shape();
-                    sh.nBands = 3;
-                    outim.ReAllocate(sh);
-                    outim.ClearPixels();
-                    MotionToColor(im, outim, maxmotion);
-                    
-                    char outname[256] = { 0 };
-                    strcpy(outname, outputPngFilename.c_str());
-                    // causes runtime error in debug build on Windows
-                    WriteImageVerb(outim, outname, verbose);
+                    flow_image.ReadFromFloFile(flowname);
+                    cv::Mat outim;
+                    flow_image.GetColorFlowImage(outim, maxmotion);
+                    cv::imwrite(outputPngFilename.c_str(), outim);
                 }
             }
             else
